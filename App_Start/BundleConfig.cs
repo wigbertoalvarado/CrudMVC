5ba3602d3169db20de1cebc8ff72a40c0e64e67b
wigbertoalvarado <alvaradotin061291@hotmail.com>
2020-09-07 09:12:04 -0600

    Completando cambios

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCCrud
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles) 
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/respond").
+                Include("~/Script/respond.js"));
+        }
+    }
+}
